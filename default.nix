065db723a5c8ed3d8dcd087590ff910aac61d830
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2020-01-27 16:45:05 +0100

    yet another change to how python overlay is setup

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 self: super:
 
 let
-  build = super.python3Packages.buildPythonPackage;
+  build = self.python3Packages.buildPythonPackage;
   ps = self.python3Packages;
 
   buildnotify = build rec {
@@ -11,12 +11,9 @@ let
       url = "https://files.pythonhosted.org/packages/76/55/d0e1a56c106ec3fed0be5d14cf9f19b55aa452ba09aacc921300f3610ea9/BuildNotify-1.0.4.tar.gz";
       sha256 = "0642471d3b10fe51cd154f0982c32d1a4ee8c3c477497ef42a87332bf41c7173";
     };
-
     # No tests included
     doCheck = false;
-
-    propagatedBuildInputs = with self.python3Packages; [ pyqt5 pytz keyring dateutil ];
-
+    propagatedBuildInputs = with ps; [ pyqt5 pytz keyring dateutil ];
     meta = {
       homepage = https://pypi.org/project/BuildNotify/;
       description = "ruise Control build monitor for Windows/Linux/Mac";
@@ -25,24 +22,16 @@ let
     };
   }; # buildnotify
 
-  mendeleev = super.python3Packages.buildPythonPackage rec {
+  mendeleev = build rec {
     pname = "mendeleev";
     version = "0.5.1";
     src = ps.fetchPypi {
       inherit pname version;
       sha256 = "0ij2g4pp3r84n6j379h7105zg33zdwh6j5an7g0slzip5nkd45w8";
     };
-    #src = super.pkgs.fetchurl {
-    #  url = "https://github.com/lmmentel/mendeleev/archive/master.zip";
-    #  #sha256 = "0n04rvpahpnm77kxcpfbdhfq45bm8958mbgdsa71ij5s5ks3kmn9";
-    #  sha256 = "114hh617v5iis2cp3dvkh13qzvrjnpr229d768rsak473p412af9";
-    #};
-
     # No tests included
     doCheck = false;
-
-    propagatedBuildInputs = with self.python3Packages; [ sqlalchemy pyfiglet pandas colorama ];
-
+    propagatedBuildInputs = with ps; [ sqlalchemy pyfiglet pandas colorama ];
     meta = {
       homepage = https://pypi.org/project/mendeleev/;
       description = "";
@@ -50,50 +39,6 @@ let
       maintainers = [ ];
     };
   }; # mendeleev
-
-  py2neo = build rec {
-    pname = "py2neo";
-    version = "4.3.0";
-    src = ps.fetchPypi {
-      inherit pname version;
-      sha256 = "08lj465d68zjbis7z9bjf4hh01phh2nynx3bm87qbqrnnsscq652";
-    };
-    propagatedBuildInputs = with ps; [ self.neo4j urllib3 pygments231 prompt_toolkit colorama pytz neotime click neobolt ];
-    doCheck = false;
-    meta = {
-      homepage = "https://py2neo.org/v4/";
-      description = "Py2neo is a client library and toolkit for working with Neo4j from within Python applications and from the command line";
-    };
-  }; # py2neo
-
-  pygments231 = build rec {
-    pname = "Pygments";
-    version = "2.3.1";
-    src = ps.fetchPypi {
-      inherit pname version;
-      sha256 = "0ji87g09jph8jqcvclgb02qvxasdnr9pzvk90rl66d90yqcxmyjz";
-    };
-    doCheck = false;
-    meta = {
-      homepage = "";
-      description = "";
-    };
-  }; # pygments 231
-
-  neotime = build rec {
-    pname = "neotime";
-    version = "1.7.4";
-    src = ps.fetchPypi {
-      inherit pname version;
-      sha256 = "1sv3fdnshlkcjy6xpdg01pr43n9bvqv356m75zg09q141yx7f12f";
-    };
-    doCheck = false;
-    propagatedBuildInputs = with ps; [ six pytz ];
-    meta = {
-      homepage = "";
-      description = "";
-    };
-  }; # neotime
 
   neobolt = build rec {
     pname = "neobolt";
@@ -110,16 +55,63 @@ let
     };
   }; # neotime
 
-in  rec {
-  # python3 overlay with missing packages
-  python3Packages = super.python3Packages //
-  {
-    #inherit buildnotify;
-    #inherit mendeleev;
-    inherit py2neo;
-    inherit pygments231;
-    inherit neotime;
-    inherit neobolt;
+  neotime = build rec {
+    pname = "neotime";
+    version = "1.7.4";
+    src = ps.fetchPypi {
+      inherit pname version;
+      sha256 = "1sv3fdnshlkcjy6xpdg01pr43n9bvqv356m75zg09q141yx7f12f";
+    };
+    doCheck = false;
+    propagatedBuildInputs = with ps; [ six pytz ];
+    meta = {
+      homepage = "";
+      description = "";
+    };
+  }; # neotime
+
+  pygments231 = build rec {
+    pname = "Pygments";
+    version = "2.3.1";
+    src = ps.fetchPypi {
+      inherit pname version;
+      sha256 = "0ji87g09jph8jqcvclgb02qvxasdnr9pzvk90rl66d90yqcxmyjz";
+    };
+    doCheck = false;
+    meta = {
+      homepage = "";
+      description = "";
+    };
+  }; # pygments 231
+
+  py2neo = build rec {
+    pname = "py2neo";
+    version = "4.3.0";
+    src = ps.fetchPypi {
+      inherit pname version;
+      sha256 = "08lj465d68zjbis7z9bjf4hh01phh2nynx3bm87qbqrnnsscq652";
+    };
+    propagatedBuildInputs = with ps; [ self.neo4j urllib3 pygments231 prompt_toolkit colorama pytz neotime click neobolt ];
+    doCheck = false;
+    meta = {
+      homepage = "https://py2neo.org/v4/";
+      description = "Py2neo is a client library and toolkit for working with Neo4j from within Python applications and from the command line";
+    };
+  }; # py2neo
+
+
+in {
+
+  python3 = super.python3.override {
+    packageOverrides = pself: psuper: {
+      inherit buildnotify;
+      inherit mendeleev;
+      inherit py2neo;
+      inherit pygments231;
+      inherit neotime;
+      inherit neobolt;
+    };
   };
+
 }
 
